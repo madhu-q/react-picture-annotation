8b04eaff933f1c345eb6031194ea185dfd8951b4
Saurabh Siwa <43809002+saurabhQuantiphi@users.noreply.github.com>
2020-01-13 19:12:29 +0530

    removed mouse event from the image

--- src/annotation/DraggingAnnotationState.ts
+++ src/annotation/DraggingAnnotationState.ts
@@ -2,22 +2,35 @@ import { ReactPictureAnnotation } from "index";
 import { IAnnotationState } from "./AnnotationState";
 import { DefaultAnnotationState } from "./DefaultAnnotationState";
 
+// export default class DraggingAnnotationState implements IAnnotationState {
+//   private context: ReactPictureAnnotation;
+//   constructor(context: ReactPictureAnnotation) {
+//     this.context = context;
+//   }
+//   public onMouseDown = () => undefined;
+//   public onMouseMove = (positionX: number, positionY: number) => {
+//     const { shapes } = this.context;
+//     const currentShape = shapes[shapes.length - 1];
+//     currentShape.onDrag(positionX, positionY);
+//   };
+
+//   public onMouseUp = () => {
+//     const { setAnnotationState } = this.context;
+//     setAnnotationState(new DefaultAnnotationState(this.context));
+//   };
+
+//   public onMouseLeave = () => this.onMouseUp();
+// }
+
 export default class DraggingAnnotationState implements IAnnotationState {
   private context: ReactPictureAnnotation;
   constructor(context: ReactPictureAnnotation) {
     this.context = context;
   }
   public onMouseDown = () => undefined;
-  public onMouseMove = (positionX: number, positionY: number) => {
-    const { shapes } = this.context;
-    const currentShape = shapes[shapes.length - 1];
-    currentShape.onDrag(positionX, positionY);
-  };
+  public onMouseMove = () => undefined;
 
-  public onMouseUp = () => {
-    const { setAnnotationState } = this.context;
-    setAnnotationState(new DefaultAnnotationState(this.context));
-  };
+  public onMouseUp = () => undefined;
 
   public onMouseLeave = () => this.onMouseUp();
 }
